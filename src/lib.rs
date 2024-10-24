b22739c72c1d2ebb4c090f981b053ebeee15ef5f
Robert O'Shea <robertosheagamedev@gmail.com>
2024-10-24 23:17:15 +0100

    Add settings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod assets;
 pub mod player;
+pub mod settings;
 pub mod shader;
---
+++ src/settings.rs
@@ -0,0 +1,13 @@
+pub struct GameSettings {
+    pub player_move_speed_x: f32,
+    pub player_move_speed_y: f32,
+}
+
+impl Default for GameSettings {
+    fn default() -> Self {
+        GameSettings {
+            player_move_speed_x: 1.0,
+            player_move_speed_y: 1.0,
+        }
+    }
+}
